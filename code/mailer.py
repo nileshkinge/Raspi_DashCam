e9bbe462489d57b52fc4363aa3b6b3540a5be3bb
Nilesh Kinge <nkinge@outlook.com>
2021-05-08 08:55:51 -0400

    exception corrected.

--- code/mailer.py
+++ code/mailer.py
@@ -5,13 +5,12 @@ from subprocess import check_output
 def IsConnectionAvailable():
     wifi_ip = check_output(['hostname', '-I'])
     if wifi_ip is not None:
-        #logMessage = "Connected to wi-fi, ip is : " + str(wifi_ip) 
-        logMessage.info("Connected to wi-fi")
+        loggerHelper.info("Connected to wi-fi, ip is : " + str(wifi_ip))
 
         return True
 
 if IsConnectionAvailable():
     mail.init()
 else:
-    logMessage.info("No Internet connection available.")
+    loggerHelper.info("No Internet connection available.")
 
