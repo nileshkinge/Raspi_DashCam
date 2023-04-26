260d1e742eed9aeefbd24861a303c7949f5032ec
nilesh kinge <nkinge@outlook.com>
2023-04-25 23:31:47 -0400

    moving code in try

--- code/connectionUtility.py
+++ code/connectionUtility.py
@@ -7,17 +7,17 @@ import configHelper
 from subprocess import check_output, call, STDOUT, CalledProcessError
 
 def connectedToknownSSID():
+    isConnected = False
     try:
         ssid = check_output(['iwgetid --raw'], stderr=STDOUT, shell=True).decode("utf-8")
+        ssids = ssid.split()
+        knowns = configHelper.getConfigSetting('knownSSID')
+
+        if(knowns.split(',').count(ssids[0]) > 0):
+            isConnected = True
+			
     except CalledProcessError as e:
         loggerHelper.info("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
-    
-    ssids = ssid.split()
-    knowns = configHelper.getConfigSetting('knownSSID')
-
-    isConnected = False
-    if(knowns.split(',').count(ssids[0]) > 0):
-        isConnected = True
 
     loggerHelper.info(("Connected" if isConnected else 'Not connected' ) + " to wi-fi from knownSSID in config")
     return isConnected
@@ -25,12 +25,13 @@ def connectedToknownSSID():
 def IsConnectionAvailable():
     try:
         wifi_ip = check_output(['hostname -I'], stderr=STDOUT, shell=True)
+        if wifi_ip is not None:
+            loggerHelper.info("Connected to wi-fi, ip is : " + str(wifi_ip))
+
+        return True
     except CalledProcessError as e:
         loggerHelper.info("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
 
-    if wifi_ip is not None:
-        loggerHelper.info("Connected to wi-fi, ip is : " + str(wifi_ip))
-
-        return True
+    return False
 
 # connectedToknownSSID()
